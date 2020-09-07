66faa6203f2eb2e400f9350fa31a5c965a005932
Hudson Daniel <hudsonprem97@gmail.com>
2020-09-07 23:16:30 +0530

    Updates

---
+++ precache-manifest.573858ba28c0aed1f4a83350f001f273.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f764c66a8f527846b0a104f0e2bc55ba",
+    "url": "/Notekeeper/index.html"
+  },
+  {
+    "revision": "535cdaf8cc1b586835a4",
+    "url": "/Notekeeper/static/js/2.0e3c4614.chunk.js"
+  },
+  {
+    "revision": "922e6bb3fc7f0c634a23",
+    "url": "/Notekeeper/static/js/main.4dfb33b4.chunk.js"
+  },
+  {
+    "revision": "8c4d29e197ec6bd2d9ee",
+    "url": "/Notekeeper/static/js/runtime-main.61d56959.js"
+  }
+]);
